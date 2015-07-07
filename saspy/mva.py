eead7c9b53a8b911cda12e4d58ff1384f4613000
Jared Dean <Jared.Dean@sas.com>
2015-07-07 10:48:57 -0400

    inital push of saspy files updates to kernel.py after resolution of formatting error

---
+++ saspy/mva.py
@@ -0,0 +1,108 @@
+import pysas
+
+def startsas(path=""):
+   pid = pysas.startsas(path)
+   return pid
+
+def getlst():
+   lstf = ""
+   x=0
+   eof = 0
+   bof = False
+   lenf = 0
+
+   while True:
+       x +=1
+       lst = pysas.getlst()
+
+       if len(lst) > 0:
+
+          lstf += lst
+          if ((not bof) and lst.count("<!DOCTYPE html>", 0, 20) > 0):
+             bof = True
+       else:
+          lenf = len(lstf)
+
+          if (lenf > 15):
+             eof = lstf.count("</html>", (lenf - 15), lenf)
+
+          if (eof > 0):
+                break
+
+          if (x > 1000 and (not bof)):
+             break
+          else:
+             continue
+
+   return lstf
+
+def getlsttxt():
+   lstf = ""
+   eof = 0
+   submit("data _null_;file print;put 'tom was here';run;", "text")
+
+   while True:
+       lst = pysas.getlst()
+
+       if len(lst) > 0:
+          lstf += lst
+          lf = len(lstf)
+          eof = lstf.find("tom was here", lf - 25, lf)
+
+          if (eof != -1):
+             final = lstf.partition("tom was here")
+             lstf = final[0]
+             break
+          else:
+             continue
+
+   return lstf
+
+def getlog():
+   logf = ""
+   while True:
+       log = pysas.getlog()
+       if len(log) > 0:
+          logf += log
+       else:
+          break
+
+   return logf
+
+
+def submit(code, results="html"):
+   odsopen  = "ods listing close;ods html5 file=stdout options(svg_mode='inline'); ods graphics on / outputfmt=svg;"
+   odsclose = "ods html5 close;ods listing;"
+   ods      = True;
+   htm      = "html HTML"
+
+   if (htm.find(results) < 0):
+      ods = False
+
+   if (ods):
+      pysas.submit(odsopen)
+
+   rc = pysas.submit(code)
+
+   if (ods):
+       pysas.submit(odsclose)
+
+   return rc
+
+def endsas():
+   return pysas.endsas()
+
+
+
+if __name__ == "__main__":
+    import pysas
+
+    startsas()
+
+    submit(sys.argv[1], "text")
+
+    print(getlog())
+    print(getlsttxt())
+
+    endsas()
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from distutils.command.install import install
+from distutils import log
+import json
+import os
+import sys
+
+with open('README.rst') as f:
+    readme = f.read()
+
+svem_flag = '--single-version-externally-managed'
+if svem_flag in sys.argv:
+    # Die, setuptools, die.
+    sys.argv.remove(svem_flag)
+
+setup(name='pysas',
+      version='0.1',
+      description='A SAS interpreter for Python',
+      long_description=readme,
+      author='Jared Dean',
+      author_email='jared.dean@sas.com',
+      #url='https://github.com/takluyver/bash_kernel',
+      packages=['saspy'],
+      cmdclass={},
+      install_requires=[],
+      classifiers = [
+      #    'License :: OSI Approved :: BSD License',
+      #    'Programming Language :: Python :: 3',
+      #    'Topic :: System :: Shells',
+      ]
+)
