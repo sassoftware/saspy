1bfb059cfe763db824ac23c02fafa4a1062c2d22
Jared Dean <Jared.Dean@sas.com>
2015-07-17 12:19:00 -0400

    update of new methods from Tom pysas34.py initial push of STAT methods

--- saspy/pysas34.py
+++ saspy/pysas34.py
@@ -9,26 +9,25 @@ saspid = None
 def getdata(table, libref="work"):
    return sasdata(libref, table)
 
-def startsas(path=""):
-   global saspid 
-   defpath ="/opt/sasinside/SASHome"
-
-   if len(path) == 0:
-      path = defpath
+def startsas(path="/opt/sasinside/SASHome"):
+   global saspid
 
    parms  = [path+"/SASFoundation/9.4/sas"]
    parms += ["-set", "TKPATH", path+"/SASFoundation/9.4/sasexe:"+path+"/SASFoundation/9.4/utilities/bin"]
    parms += ["-set", "SASROOT", path+"/SASFoundation/9.4"]
    parms += ["-set", "SASHOME", path]
+   parms += ["-pagesize", "MAX"]
    parms += ["-stdio"]
 
    saspid = subprocess.Popen(parms, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    fcntl.fcntl(saspid.stdout, fcntl.F_SETFL, os.O_NONBLOCK)
    fcntl.fcntl(saspid.stderr,fcntl. F_SETFL, os.O_NONBLOCK)
-  
-   submit("options svgtitle='svgtitle';", "text")
-     
+
+   submit("options svgtitle='svgtitle'; options validvarname=any;", "text")
+
    return saspid.pid
+
+
 
 def getlog(wait=5):
    #import pdb; pdb.set_trace()
@@ -53,6 +52,7 @@ def getlog(wait=5):
 
    return logf.decode()
 
+
 def getlst(wait=5):
    #import pdb; pdb.set_trace()
    lstf = b''
@@ -65,18 +65,18 @@ def getlst(wait=5):
       lst = saspid.stdout.read1(4096)
       if len(lst) > 0:
          lstf += lst
-                          
+
          if ((not bof) and lst.count(b"<!DOCTYPE html>", 0, 20) > 0):
             bof = True
       else:
          lenf = len(lstf)
-   
+
          if (lenf > 15):
             eof = lstf.count(b"</html>", (lenf - 15), lenf)
-   
+
          if (eof > 0):
                break
-         
+
          if not bof:
             quit -= 1
             if quit < 0:
@@ -85,42 +85,43 @@ def getlst(wait=5):
 
    return lstf.decode()
 
-def getlsttxt(wait=5):
+
+def getlstlog(done='used (Total process time):', count=1):
    #import pdb; pdb.set_trace()
    lstf = b''
-   quit = wait * 2
-   eof = 0
-   submit("data _null_;file print;put 'tom was here';run;", "text")
+   logf = b''
+   quit = False
+   eof = 5
 
    while True:
-      #try:
-      #   lst = saspid.stdout.read(4096)
-      #except IOError as e:
-
-      lst = saspid.stdout.read1(4096)
-      if len(lst) > 0:
+      if quit:
+         eof -= 1
+      if eof < 0:
+         break
+      lst = saspid.stdout.read(-1)
+      #if len(lst) > 0:
+      if lst != None:
          lstf += lst
-
-         lenf = len(lstf)
-         eof = lstf.find(b"tom was here", lenf - 25, lenf)
-   
-         if (eof != -1):
-            final = lstf.partition(b"tom was here")
-            f2 = final[0].decode().rpartition(chr(12))
-            break
+         print("=====================LST==============\n"+lst.decode()+"\n\n\n\n")
       else:
-         quit -= 1
-         if quit < 0:
-            break
-         sleep(0.5)
+         log = saspid.stderr.read(-1)
+         #if len(log) > 0:
+         if log != None:
+            logf += log
+            print("=====================LOG==============\n"+log.decode()+"\n\n\n\n")
+            if logf.count(done.encode()) >= count:
+               quit = True
 
-   return f2[0]
+   return lstf.decode()
+
+
 
 
 def submit(code, results="html"):
    #import pdb; pdb.set_trace()
-   odsopen  = b"ods listing close;ods html5 file=stdout options(svg_mode='inline'); ods graphics on / outputfmt=svg;\n"
-   odsclose = b"ods html5 close;ods listing;\n"
+   #odsopen  = b"ods listing close;ods html5 file=stdout options(svg_mode='inline'); ods graphics on / outputfmt=svg;\n"
+   odsopen  = b"ods listing close; ods html5 file=stdout options(bitmap_mode='inline') device=png; ods graphics on / outputfmt=png;\n"
+   odsclose = b" ;*\';*\";*/; ods html5 close;ods listing;\n"
    ods      = True;
    htm      = "html HTML"
 
@@ -139,6 +140,7 @@ def submit(code, results="html"):
 
    return out
 
+
 def endsas():
    code = b"\n;quit;endsas;\n"
    saspid.stdin.write(code)
@@ -152,7 +154,7 @@ class sasdata:
         failed = 0
         if out == "HTML":
            try:
-              from IPython.display import HTML 
+              from IPython.display import HTML
            except:
               failed = 1
 
@@ -163,7 +165,7 @@ class sasdata:
         else:
            self.HTML = 0
 
-        self.libref = libref 
+        self.libref = libref
         self.table  = table
 
     def __flushlst__(self):
@@ -180,17 +182,54 @@ class sasdata:
         code += "(obs="
         code += str(obs)
         code += ");run;"
-        
+
         self.__flushlst__()
 
         if self.HTML:
-           from IPython.display import HTML 
+           from IPython.display import HTML
            submit(code)
            return HTML(getlst())
         else:
            submit(code, "text")
            print(getlsttxt())
+
    
+    def tail(self, obs=5):
+        #import pdb; pdb.set_trace()
+        code  = "%put lastobs=%sysfunc(attrn(%sysfunc(open("
+        code += self.libref
+        code += "."
+        code += self.table
+        code += ")),NOBS));"
+
+        getlog()
+        submit(code, "text")
+        log = getlog()
+
+        lastobs = log.rpartition("lastobs=")
+        lastobs = lastobs[2].partition(" ")
+        lastobs = int(lastobs[0])
+
+        code  = "proc print data="
+        code += self.libref
+        code += "."
+        code += self.table
+        code += "(firstobs="
+        code += str(lastobs-(obs-1))
+        code += " obs="
+        code += str(lastobs)
+        code += ");run;"
+
+        self.__flushlst__()
+
+        if self.HTML:
+           from IPython.display import HTML
+           submit(code)
+           return HTML(getlst())
+        else:
+           submit(code, "text")
+           print(getlsttxt())
+
     def contents(self):
         code  = "proc contents data="
         code += self.libref
@@ -201,24 +240,25 @@ class sasdata:
         self.__flushlst__()
 
         if self.HTML:
-           from IPython.display import HTML 
+           from IPython.display import HTML
            submit(code)
            return HTML(getlst())
         else:
            submit(code, "text")
            print(getlsttxt())
+
    
     def means(self):
         code  = "proc means data="
         code += self.libref
         code += "."
         code += self.table
-        code += ";run;"
-        
+        code += " n mean std min p25 p50 p75 max;run;"
+
         self.__flushlst__()
 
         if self.HTML:
-           from IPython.display import HTML 
+           from IPython.display import HTML
            submit(code)
            return HTML(getlst())
         else:
@@ -228,6 +268,99 @@ class sasdata:
 def getdata(table, libref="work"):
    return sasdata(libref, table)
 
+def dataframe2sasdata(df, table='a', libref="work", out='HTML'):
+   #import pdb; pdb.set_trace()
+   input = ""
+   card  = ""
+
+   for name in range(len(df.columns)):
+      input += "'"+df.columns[name]+"'n "
+      if str(df.dtypes[name]) == 'character' or str(df.dtypes[name]) == 'object':
+         input += "$ "
+
+   submit("data "+libref+"."+table+";\n input "+input+";\n datalines;\n", "text")
+
+   for row in df.iterrows():
+      card  = ""
+      for col in range(len(row[1])):
+         card += str(row[1][col])+" "
+      submit(card, "text")
+
+   submit(";run;", "text")
+
+   return sasdata(libref, table, out=out)
+
+def sd2df(sd):
+    return sasdata2dataframe(sd)
+
+def sasdata2dataframe(sd):
+   #import pdb; pdb.set_trace()
+   import pandas as pd
+   import socket as socks
+   datar = ""
+
+
+   code  = "data _null_; file STDERR;d = open('"
+   code += sd.libref
+   code += "."
+   code += sd.table
+   code += "');\n lrecl = attrn(d, 'LRECL'); nvars = attrn(d, 'NVARS'); lr='LRECL='; vn='VARNUMS='; vl='VARLIST=';\n"
+   code += "put lr lrecl; put vn nvars; put vl;\n"
+   code += "do i = 1 to nvars; var = varname(d, i); put var; end; run;"
+
+   getlog()
+   submit(code, "text")
+   log = getlog()
+
+
+   l2 = log.rpartition("LRECL= ")
+   l2 = l2[2].partition("\n")
+   lrecl = int(l2[0])
+
+   l2 = l2[2].partition("VARNUMS= ")
+   l2 = l2[2].partition("\n")
+   nvars = int(l2[0])
+
+   l2 = l2[2].partition("\n")
+   varlist = l2[2].split("\n", nvars)
+   del varlist[nvars]
+
+   sock = socks.socket()
+   sock.bind(("",0))
+   port = sock.getsockname()[1]
+
+   code  = "data _null_; x = sleep(1,1);run;\n"
+   code += "filename sock socket ':"+str(port)+"' lrecl=32767 recfm=v termstr=LF;\n"
+   code += " data _null_; set "+sd.libref+"."+sd.table+";\n file sock; put "
+   for i in range(len(varlist)):
+      code += "'"+varlist[i]+"'n "
+      if i < (len(varlist)-1):
+         code += "'09'x "
+   code += "; run;\n"
+
+   sock.listen(0)
+   submit(code, 'text')
+   newsock = sock.accept()
+      while True:
+      data = newsock[0].recv(4096)
+      if len(data):
+         datar += data.decode()
+      else:
+         break
+
+   newsock[0].shutdown(socks.SHUT_RDWR)
+   newsock[0].close()
+   sock.close()
+
+   r = []
+   for i in datar.splitlines():
+      r.append(tuple(i.split(sep='\t')))
+
+   df = pd.DataFrame.from_records(r, columns=varlist)
+
+   return df
+   #return datar
+   #return pd.DataFrame(data=datar, columns=varlist)
 
 def read_csv(file, table, libref="work"):
    code  = "filename x url \""+file+"\";\n"
@@ -247,4 +380,3 @@ if __name__ == "__main__":
     print(getlsttxt())
 
     endsas()
-
---
+++ saspy/sasstat.py
@@ -0,0 +1,182 @@
+from IPython.core.display import HTML
+from SAS import pysas34 as sas
+import time
+import logging
+
+# create logger
+logger = logging.getLogger('')
+logger.setLevel(logging.WARN)
+
+
+class sasstat:
+    def __init__(self, *args, **kwargs):
+        '''Submit an initial set of macros to prepare the SAS system'''
+        code="options pagesize=max; %include '/root/jared/metis/saspy_pip/saspy/libname_gen.sas'; "
+        sas.submit(code,"text")
+        logger.debug("Initalization of SAS Macro: " + str(sas.getlog()))
+
+    def __flushlst__(self):
+        lst = b'hi'
+        while(len(lst) > 0):
+           lst = saspid.stdout.read1(4096)
+           continue
+
+    def _objectmethods(self,obj,*args):
+        self.obj=obj
+        clear=sas.getlog(1)
+        code  ="%listdata("
+        code +=self.obj
+        code +=");"
+        logger.debug("Object Method macro call: " + str(code))
+        sas.submit(code,"text")
+        meth=sas.getlog().splitlines()
+        logger.debug('SAS Log: ' + str(meth))
+        objlist=meth[meth.index('startparse9878')+1:meth.index('endparse9878')]
+        logger.debug("PROC attr list: " + str(objlist))
+        return objlist
+
+    def hpsplit(self, model='', data=None, **kwargs):
+        self.model=model
+        self.data=data
+        self.objtype='hps'
+        self.objname='hps1' #how to give this a better name -- translate to a libname so needs to be less than 8
+        code  = "%macro proccall(d);\n"
+        code += "proc hpsplit data="
+        code += self.data.libref
+        code += "."
+        code += self.data.table
+        code += " plots=all ;"
+        if len(model):
+            code += "model "+self.model+";"
+        code += "run; "
+        code += "%mend;\n"
+        code += "%mangobj("
+        code += self.objname
+        code += ","
+        code += self.objtype
+        code += ","
+        code += self.data.table
+        code += ");"
+
+        logger.debug("HPSPLIT macro submission: " + str(code))
+        sas.submit(code,"text")
+        #time.sleep(5)
+        try:
+            obj1=self._objectmethods(self.objname)
+            #print("in try block")
+        except Exception:
+            #print("Exception Block:", sys.exc_info()[0])
+            obj1=[]
+
+        return (Results(obj1,self.objname))
+
+    def reg(self, model='', data=None, **kwargs):
+        self.model=model
+        self.data=data
+        self.objtype='reg'
+        self.objname='reg1' #how to give this a better name
+        code  = "%macro proccall(d);\n"
+        code += "proc reg plots(unpack)=all data="
+        code += self.data.libref
+        code += "."
+        code += self.data.table
+        code += ";"
+        if len(model):
+            code += "model "+self.model+";"
+        code += "run; quit;"
+        code += "%mend;\n"
+        code += "%mangobj("
+        code += self.objname
+        code += ","
+        code += self.objtype
+        code += ","
+        code += self.data.table
+        code += ");"
+
+        logger.debug("REG macro submission: " + str(code))
+        sas.submit(code,"text")
+        #time.sleep(.2)
+        try:
+            obj1=self._objectmethods(self.objname)
+            #print(obj1)
+        except Exception:
+            obj1=[]
+        return (Results(obj1,self.objname))
+
+    def glm(self, model='', data=None, **kwargs):
+        self.model=model
+        self.data=data
+        self.cls=kwargs.get('cls', '')
+        self.objtype='glm'
+        self.objname='glm1' #how to give this a better name
+        code  = "%macro proccall(d);\n"
+        code += "proc glm plots(unpack)=all data="
+        code += self.data.libref
+        code += "."
+        code += self.data.table
+        code += ";"
+        if len(self.cls):
+            code += "class %s;" % (self.cls)
+        if len(model):
+            code += "model "+self.model+";"
+        code += "run; quit;"
+        code += "%mend;\n"
+        code += "%mangobj("
+        code += self.objname
+        code += ","
+        code += self.objtype
+        code += ","
+        code += self.data.table
+        code += ");"
+
+        logger.debug("GLM macro submission: " + str(code))
+        sas.submit(code,"text")
+        try:
+            obj1=self._objectmethods(self.objname)
+        except Exception:
+            obj1=[]
+        return (Results(obj1,self.objname))
+
+from collections import namedtuple
+
+class Results(object):
+    '''Return results from a SAS Model object'''
+    def __init__(self, attrs, objname):
+
+        self._attrs = attrs
+        self._name = objname
+        #logger.debug("attributes passed to Results: "+ self._attrs,self._name)
+
+    def __dir__(self):
+        '''Overload dir method to return the attributes'''
+        return self._attrs
+
+        def __getattr__(self, attr):
+        if attr.startswith('_'):
+            return getattr(self, attr)
+        if attr.upper() in self._attrs:
+            #print(attr.upper())
+            data = self._go_run_code(attr)
+            '''
+            if not attr.lower().endswith('plot'):
+                libname, table = data.split()
+                table_data = sasdata(libname, table)
+                content = table_data.contents()
+                # parse content
+                headers = content[0]
+
+                res = namedtuple('SAS Result', headers)
+                results = [ res(x) for x in headers[1:] ]
+            '''
+
+        else:
+             raise AttributeError
+        return HTML(data)
+
+    def _go_run_code(self, attr):
+        #print(self._name, attr)
+        code = '%%getdata(%s, %s);' % (self._name, attr)
+        #print (code)
+        sas.submit(code)
+        return sas.getlst()
+
