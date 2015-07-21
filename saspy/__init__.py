01cd266290937e40d9223d4bf0b69fff2b688f53
Jared Dean <Jared.Dean@sas.com>
2015-07-21 15:09:47 -0400

    update init.py to load correct file
    covert pysas.py to sasbase.py and add sasstat.py

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -13,4 +13,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-from saspy import pysas
+from saspy import pysas34
+from saspy import sasstat
