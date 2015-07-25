792c27e7881e83b72239cad99683ab3ab58807dc
Jared Dean <Jared.Dean@sas.com>
2015-07-25 09:40:02 -0400

    updates to __init__.py

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -1,2 +1,5 @@
-from saspy import pysas34
-from saspy import sasstat
+from saspy.pysas34 import *
+from saspy.sasstat import *
+SAS = sas_session()       
+sas = SAS
+print("SAS session available as 'SAS'. Pid="+str(sas._startsas()))
