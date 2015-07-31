17b686317235a5a4b55b3397198a98332418e566
Jared Dean <Jared.Dean@sas.com>
2015-07-31 13:04:36 -0400

    minor fixes in files to enable the sasets method

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -15,6 +15,7 @@
 #
 from saspy.pysas34 import *
 from saspy.sasstat import *
+from saspy.sasets  import *
 SAS = SAS_session()       
 sas = SAS
 print("SAS session available as 'SAS'. Pid="+str(sas._startsas()))
