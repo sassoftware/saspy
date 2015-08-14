06cfc78e621636c1cfa5c64f3f7b899444f7c9c4
Jared Dean <Jared.Dean@sas.com>
2015-08-14 09:56:55 -0400

    refactor code generation using timeseries. other methods need to be adjusted

--- saspy/sasets.py
+++ saspy/sasets.py
@@ -31,117 +31,123 @@ class SAS_ets:
         logger.debug("PROC attr list: " + str(objlist))
         return objlist
 
-    def _makeProccallMacro(self, objtype, objname, data='', by='', corr='',
-                           crosscorr='', decomp='', id='', season='', trend='', var='',
-                           crossvar='', identify='', estimate='', outlier='', forecast='', 
-                           autoreg='', blockseason='', cycle='', deplag='', irregular='', 
-                           level='', model='', nloptions='', performance='', randomreg='', 
-                           slope='', splinereg='', splineseason='', fcmport='', outarrays='', 
-                           outscalars='', prog_stmts=''):
+    def _makeProccallMacro(self, objtype, objname, data=None, args=''):
+        #by='', corr='',
+        #                   crosscorr='', decomp='', id='', season='', trend='', var='',
+        #                   crossvar='', identify='', estimate='', outlier='', forecast='', 
+        #                   autoreg='', blockseason='', cycle='', deplag='', irregular='', 
+        #                   level='', model='', nloptions='', performance='', randomreg='', 
+        #                   slope='', splinereg='', splineseason='', fcmport='', outarrays='', 
+        #                   outscalars='', prog_stmts=''):
         code  = "%macro proccall(d);\n"
         code += "proc %s data=%s.%s plots=all;\n" % (objtype, data.libref, data.table)
-        if by:
-            logger.debug("by statement,length: %s,%s", by, len(by))
-            code += "by %s;" % (by, )
-        if corr:
-            logger.debug("corr statement,length: %s,%s", corr, len(corr))
-            code += "corr %s;" % (corr, )
-        if crosscorr:
-            logger.debug("crosscorr statement,length: %s,%s", crosscorr, len(crosscorr))
-            code += "crosscorr %s;" % (crosscorr, )
-        if decomp:
-            logger.debug("decomp statement,length: %s,%s", decomp, len(decomp))
-            code += "decomp %s;" % (decomp, )
-        if id:
-            logger.debug("id statement,length: %s,%s", id, len(id))
-            code += "id %s;" % (id, )
-        if season:
-            logger.debug("season statement,length: %s,%s", season, len(season))
-            code += "season %s;" % (season, )
-        if trend:
-            logger.debug("trend statement,length: %s,%s", trend, len(trend))
-            code += "trend %s;" % (trend, )
-        if var:
-            logger.debug("var statement,length: %s,%s", var, len(var))
-            code += "var %s;" % (var, )
-        if crossvar:
-            logger.debug("crossvar statement,length: %s,%s", crossvar, len(crossvar))
-            code += "crossvar %s;" % (crossvar, )
-        if identify:
-            logger.debug("identify statement,length: %s,%s", identify, len(identify))
-            code += "identify %s;" % (identify, )
-        if estimate:
-            logger.debug("estimate statement,length: %s,%s", estimate, len(estimate))
-            code += "estimate %s;" % (estimate, )
-        if outlier:
-            logger.debug("outlier statement,length: %s,%s", outlier, len(outlier))
-            code += "outlier %s;" % (outlier, )
-        if forecast:
-            logger.debug("forecast statement,length: %s,%s", forecast, len(forecast))
-            code += "forecast %s;" % (forecast, )
-        if autoreg:
-            logger.debug("autoreg statement,length: %s,%s", autoreg, len(autoreg))
-            code += "autoreg %s;" % (autoreg, )
-        if blockseason:
-            logger.debug("blockseason statement,length: %s,%s", blockseason, len(blockseason))
-            code += "blockseason %s;" % (blockseason, )
-        if cycle:
-            logger.debug("cycle statement,length: %s,%s", cycle, len(cycle))
-            code += "cycle %s;" % (cycle, )
-        if deplag:
-            logger.debug("deplag statement,length: %s,%s", deplag, len(deplag))
-            code += "deplag %s;" % (deplag, )
-        if irregular:
-            logger.debug("irregular statement,length: %s,%s", irregular, len(irregular))
-            code += "irregular %s;" % (irregular, )
-        if level:
-            logger.debug("level statement,length: %s,%s", level, len(level))
-            code += "level %s;" % (level, )
-        if model:
-            logger.debug("model statement,length: %s,%s", model, len(model))
-            code += "model %s;" % (model, )
-        if nloptions:
-            logger.debug("nloptions statement,length: %s,%s", nloptions, len(nloptions))
-            code += "nloptions %s;" % (nloptions, )
-        if performance:
-            logger.debug("performance statement,length: %s,%s", performance, len(performance))
-            code += "performance %s;" % (performance, )
-        if randomreg:
-            logger.debug("randomreg statement,length: %s,%s", randomreg, len(randomreg))
-            code += "randomreg %s;" % (randomreg, )
-        if slope:
-            logger.debug("slope statement,length: %s,%s", slope, len(slope))
-            code += "slope %s;" % (slope, )
-        if splinereg:
-            logger.debug("splinereg statement,length: %s,%s", splinereg, len(splinereg))
-            code += "splinereg %s;" % (splinereg, )
-        if splineseason:
-            logger.debug("splineseason statement,length: %s,%s", splineseason, len(splineseason))
-            code += "splineseason %s;" % (splineseason, )
-        if fcmport:
-            logger.debug("fcmport statement,length: %s,%s", fcmport, len(fcmport))
-            code += "fcmport %s;" % (fcmport, )
-        if outarrays:
-            logger.debug("outarrays statement,length: %s,%s", outarrays, len(outarrays))
-            code += "outarrays %s;" % (outarrays, )
-        if outscalars:
-            logger.debug("outscalars statement,length: %s,%s", outscalars, len(outscalars))
-            code += "outscalars %s;" % (outscalars, )
-        if prog_stmts:
-            logger.debug("prog_stmts statement,length: %s,%s", prog_stmts, len(prog_stmts))
-            code += " %s;" % (prog_stmts, )
+        logger.debug("args value: " + str(args))
+        logger.debug("args type: " + str(type(args)))
+        if 'by' in args:
+            logger.debug("by statement,length: %s,%s", args['by'], len(args['by']))
+            code += "by %s;\n" % (args['by'])
+        if 'corr' in args:
+            logger.debug("corr statement,length: %s,%s", args['corr'], len(args['corr']))
+            code += "corr %s;\n" % (args['corr'])
+        if 'crosscorr' in args:
+            logger.debug("crosscorr statement,length: %s,%s", args['crosscorr'], len(args['crosscorr']))
+            code += "crosscorr %s;\n" % (args['crosscorr'])
+        if 'decomp' in args:
+            logger.debug("decomp statement,length: %s,%s", args['decomp'], len(args['decomp']))
+            code += "decomp %s;\n" % (args['decomp'])
+        if 'id' in args:
+            logger.debug("id statement,length: %s,%s", args['id'], len(args['id']))
+            code += "id %s;\n" % (args['id'])
+        if 'season' in args:
+            logger.debug("season statement,length: %s,%s", args['season'], len(args['season']))
+            code += "season %s;\n" % (args['season'])
+        if 'trend' in args:
+            logger.debug("trend statement,length: %s,%s", args['trend'], len(args['trend']))
+            code += "trend %s;\n" % (args['trend'])
+        if 'var' in args:
+            logger.debug("var statement,length: %s,%s", args['var'], len(args['var']))
+            code += "var %s;\n" % (args['var'])
+        if 'crossvar' in args:
+            logger.debug("crossvar statement,length: %s,%s", args['crossvar'], len(args['crossvar']))
+            code += "crossvar %s;\n" % (args['crossvar'])
+        if 'identify' in args:
+            logger.debug("identify statement,length: %s,%s", args['identify'], len(args['identify']))
+            code += "identify %s;\n" % (args['identify'])
+        if 'estimate' in args:
+            logger.debug("estimate statement,length: %s,%s", args['estimate'], len(args['estimate']))
+            code += "estimate %s;\n" % (args['estimate'])
+        if 'outlier' in args:
+            logger.debug("outlier statement,length: %s,%s", args['outlier'], len(args['outlier']))
+            code += "outlier %s;\n" % (args['outlier'])
+        if 'forecast' in args:
+            logger.debug("forecast statement,length: %s,%s", args['forecast'], len(args['forecast']))
+            code += "forecast %s;\n" % (args['forecast'])
+        if 'autoreg' in args:
+            logger.debug("autoreg statement,length: %s,%s", args['autoreg'], len(args['autoreg']))
+            code += "autoreg %s;\n" % (args['autoreg'])
+        if 'blockseason' in args:
+            logger.debug("blockseason statement,length: %s,%s", args['blockseason'], len(args['blockseason']))
+            code += "blockseason %s;\n" % (args['blockseason'])
+        if 'cycle' in args:
+            logger.debug("cycle statement,length: %s,%s", args['cycle'], len(args['cycle']))
+            code += "cycle %s;\n" % (args['cycle'])
+        if 'deplag' in args:
+            logger.debug("deplag statement,length: %s,%s", args['deplag'], len(args['deplag']))
+            code += "deplag %s;\n" % (args['deplag'])
+        if 'irregular' in args:
+            logger.debug("irregular statement,length: %s,%s", args['irregular'], len(args['irregular']))
+            code += "irregular %s;\n" % (args['irregular'])
+        if 'level' in args:
+            logger.debug("level statement,length: %s,%s", args['level'], len(args['level']))
+            code += "level %s;\n" % (args['level'])
+        if 'model' in args:
+            logger.debug("model statement,length: %s,%s", args['model'], len(args['model']))
+            code += "model %s;\n" % (args['model'])
+        if 'nloptions' in args:
+            logger.debug("nloptions statement,length: %s,%s", args['nloptions'], len(args['nloptions']))
+            code += "nloptions %s;\n" % (args['nloptions'])
+        if 'performance' in args:
+            logger.debug("performance statement,length: %s,%s", args['performance'], len(args['performance']))
+            code += "performance %s;\n" % (args['performance'])
+        if 'randomreg' in args:
+            logger.debug("randomreg statement,length: %s,%s", args['randomreg'], len(args['randomreg']))
+            code += "randomreg %s;\n" % (args['randomreg'])
+        if 'slope' in args:
+            logger.debug("slope statement,length: %s,%s", args['slope'], len(args['slope']))
+            code += "slope %s;\n" % (args['slope'])
+        if 'splinereg' in args:
+            logger.debug("splinereg statement,length: %s,%s", args['splinereg'], len(args['splinereg']))
+            code += "splinereg %s;\n" % (args['splinereg'])
+        if 'splineseason' in args:
+            logger.debug("splineseason statement,length: %s,%s", args['splineseason'], len(args['splineseason']))
+            code += "splineseason %s;\n" % (args['splineseason'])
+        if 'fcmport' in args:
+            logger.debug("fcmport statement,length: %s,%s", args['fcmport'], len(args['fcmport']))
+            code += "fcmport %s;\n" % (args['fcmport'])
+        if 'outarrays' in args:
+            logger.debug("outarrays statement,length: %s,%s", args['outarrays'], len(args['outarrays']))
+            code += "outarrays %s;\n" % (args['outarrays'])
+        if 'outscalars' in args:
+            logger.debug("outscalars statement,length: %s,%s", args['outscalars'], len(args['outscalars']))
+            code += "outscalars %s;\n" % (args['outscalars'])
+        if 'prog_stmts' in args:
+            logger.debug("prog_stmts statement,length: %s,%s", args['prog_stmts'], len(args['prog_stmts']))
+            code += " %s;\n" % (args['prog_stmts'])   
         code += "run; quit; %mend;\n"
         code += "%%mangobj(%s,%s,%s);" % (objname, objtype,data.table)
         logger.debug("Proc code submission: " + str(code))
         return (code)
-        
+    
     def timeseries(self, **kwargs):
         '''Python method to call the TIMESERIES procedure
         Documentation link: http://support.sas.com/documentation/cdl//en/etsug/68148/HTML/default/viewer.htm#etsug_timeseries_overview.htm
         '''
+        data=kwargs.pop('data',None)
+
         objtype='timeseries'
         objname='ts1'+self.sas._objcnt()  #translate to a libname so needs to be less than 8
-        code=self._makeProccallMacro(objtype, objname, kwargs)
+        code=self._makeProccallMacro(objtype, objname, data, kwargs)
+        #logger.debug("Code type: " + type(code))
         logger.debug("TIMESERIES macro submission: " + str(code))
         self.sas._asubmit(code,"text")
         try:
