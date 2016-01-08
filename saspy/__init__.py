f236aee8ec5e90393a9b46d04ac4efd19bcd6537
Jared Dean <Jared.Dean@sas.com>
2016-01-08 16:12:16 -0500

    clean up comments

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -14,13 +14,10 @@
 #  limitations under the License.
 #
 import os
-print ("after os")
 from saspy.pysas34 import *
-print ("after pysas34")
 from saspy.sasstat import *
 from saspy.sasets  import *
 from saspy.SASLogLexer import *
-print ("after all saspy import")
 SAS = SAS_session()       
 sas = SAS
 executable = os.environ.get('SAS_EXECUTABLE', 'sas')
@@ -29,5 +26,4 @@ if executable=='sas':
 e2=executable.split('/')
 _path='/'.join(e2[0:e2.index('SASHome')+1])
 _version=e2[e2.index('SASFoundation')+1]
-print ("after exec " + _path + _version)
 print("SAS session available as 'SAS'. Pid="+str(sas._startsas(path=_path, version=_version)))
