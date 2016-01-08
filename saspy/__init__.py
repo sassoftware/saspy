5460fc7defcf8655055d387ee165f30fc9dd71fa
Jared Dean <Jared.Dean@sas.com>
2016-01-08 16:25:11 +0000

    env updates

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -13,10 +13,21 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+import os
+print ("after os")
 from saspy.pysas34 import *
+print ("after pysas34")
 from saspy.sasstat import *
 from saspy.sasets  import *
 from saspy.SASLogLexer import *
+print ("after all saspy import")
 SAS = SAS_session()       
 sas = SAS
-print("SAS session available as 'SAS'. Pid="+str(sas._startsas()))
+executable = os.environ.get('SAS_EXECUTABLE', 'sas')
+if executable=='sas':
+    executable='/opt/sasinside/SASHome/SASFoundation/9.4/sas'
+e2=executable.split('/')
+_path='/'.join(e2[0:e2.index('SASHome')+1])
+_version=e2[e2.index('SASFoundation')+1]
+print ("after exec " + _path + _version)
+print("SAS session available as 'SAS'. Pid="+str(sas._startsas(path=_path, version=_version)))
