83e665fac226a038af9fb7c8997f53bbac6dd813
Tom Weber <Tom.Weber@sas.com>
2016-02-24 13:39:14 -0500

    clean up for kernel only

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -1,14 +1,9 @@
 import os
 from saspy.pysas34 import *
-from saspy.sasstat import *
-from saspy.sasets  import *
 from saspy.SASLogLexer import *
-SAS = SAS_session()       
-sas = SAS
 executable = os.environ.get('SAS_EXECUTABLE', 'sas')
 if executable=='sas':
     executable='/opt/sasinside/SASHome/SASFoundation/9.4/sas'
 e2=executable.split('/')
 _path='/'.join(e2[0:e2.index('SASHome')+1])
 _version=e2[e2.index('SASFoundation')+1]
-print("SAS session available as 'SAS'. Pid="+str(sas._startsas(path=_path, version=_version)))
