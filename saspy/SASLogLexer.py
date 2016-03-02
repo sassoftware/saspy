18f0c50fd1a150c3d7125fcd4af514c751197f66
Jared Dean <Jared.Dean@sas.com>
2016-03-02 15:51:10 -0500

    improve lexer for multiline messages
    improve coloring for when messages span more than one line.

--- saspy/SASLogLexer.py
+++ saspy/SASLogLexer.py
@@ -1,7 +1,5 @@
-from IPython.core.display import HTML
 from pygments import highlight
 from pygments.lexer import RegexLexer
-from pygments.formatters import HtmlFormatter
 from pygments.token import *
 from pygments.style import Style
 
@@ -17,14 +15,25 @@ class SASLogStyle(Style):
 class SASLogLexer(RegexLexer):
     __all__ = ['SASLogLexer']
     name = 'Lexer to Color SAS Logs equivilent to DMS'
-
     tokens = {
         'root': [
-            (r'\d+.*$', String),
-            (r'^NOTE.*$', Comment),
-            (r'^ERROR.*$', Keyword),
-            (r'^WARNING.*$', Name)
+            (r'^\d+.*((\n|\t|\n\t)[ ]([^WEN].*)(.*))*', String),
+            (r'^NOTE.*((\n|\t|\n\t)[ ]([^WEN].*)(.*))*', Comment.Multiline, 'note'),
+            (r'^ERROR.*((\n|\t|\n\t)[ ]([^WEN].*)(.*))*', Keyword.Multiline, 'error'),
+            (r'^WARNING.*((\n|\t|\n\t)[ ]([^WEN].*)(.*))*', Name.Multiline, 'warning'),
+            (r'\s', Text)
+        ],
+        'error': [
+            (r'^\s+.*$',Keyword.Multiline),
+            (r'^\S+.*$',Keyword.Multiline,'#pop')
+        ],
+        'note': [
+            (r'^\s+.[\s\S]*',Comment.Multiline),
+            (r'^\S+.[\s\S]*',Comment.Multiline,'#pop')
+        ],
+        'warning': [
+            (r'^\s+.*$',Name.Multiline),
+            (r'^\S+.*$',Name.Multiline,'#pop')
         ]
+        
     }
-
-
