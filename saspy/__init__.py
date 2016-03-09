3d14a4e75d4aec65f6c99bd4e8778e587303de91
Tom Weber <Tom.Weber@sas.com>
2016-03-09 15:41:05 +0000

    clean up pysas invocation from kernel and init script

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -13,12 +13,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-import os
 from saspy.pysas34 import *
 from saspy.SASLogLexer import *
-executable = os.environ.get('SAS_EXECUTABLE', 'sas')
-if executable=='sas':
-    executable='/opt/sasinside/SASHome/SASFoundation/9.4/sas'
-e2=executable.split('/')
-_path='/'.join(e2[0:e2.index('SASHome')+1])
-_version=e2[e2.index('SASFoundation')+1]
