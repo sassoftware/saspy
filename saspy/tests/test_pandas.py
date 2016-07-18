e97c8d5f9e7580afb5155ab369d97327bf21f2e4
Tom Weber <Tom.Weber@sas.com>
2016-07-18 16:28:29 -0400

    create sas table to use; other wasn't always there

--- saspy/tests/test_pandas.py
+++ saspy/tests/test_pandas.py
@@ -25,14 +25,24 @@ class TestPandasDataFrameIntegration(unittest.TestCase):
         import pandas
         self.sas.set_batch(True)
 
-        td = self.sas.sasdata('timedata', libref='sashelp', results='text')
+        ll = self.sas.submit('''
+                             data testdata; format d1 date. dt1 datetime. ;
+                             d1 = '03jan1966'd; dt1='03jan1966:13:30:59'dt; output;           
+                             d1 = '03jan1966'd; dt1='03jan1966:13:30:59'dt; output;           
+                             d1 = '03jan1966'd; dt1='03jan1966:13:30:59'dt; output;           
+                             d1 = '03jan1966'd; dt1='03jan1966:13:30:59'dt; output;           
+                             d1 = '03jan1966'd; dt1='03jan1966:13:30:59'dt; output; 
+                             run;
+                             ''')
+
+        td = self.sas.sasdata('testdata', results='text')
         self.assertIsInstance(td, saspy.SASdata, msg="cars = sas.sasdata(...) failed")
 
         #test sas data to data frame
         df = td.to_df()
         self.assertIsInstance(df, pandas.core.frame.DataFrame, msg="df = td.to_df(...) failed")
         result = df.head()
-        expected = ['0', '1', '1997-07-25', '00:00:00']
+        expected = ['0', '1966-01-03', '1966-01-03', '13:30:59']
         rows = result.to_string().splitlines()
         retrieved = []
         for i in range(len(rows)):
@@ -43,7 +53,7 @@ class TestPandasDataFrameIntegration(unittest.TestCase):
         td2 = self.sas.df2sd(df, 'td2', results='text')
         self.assertIsInstance(td2, saspy.SASdata, msg="td2 = sas.df2sd((...) failed")
         ll = td2.head()
-        expected = ['1', '1', '1997-07-25T00:00:00.000000']
+        expected = ['1', '1966-01-03T00:00:00.000000', '1966-01-03T13:30:59.000000']
         rows = ll['LST'].splitlines()
         retrieved = []
         for i in range(len(rows)):
