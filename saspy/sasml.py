e27cce1f2c7349a292aaac011ff1d6668972acec
Jared Dean <Jared.Dean@sas.com>
2016-07-19 00:16:24 -0400

    add documentation for adding procedure methods

--- saspy/sasml.py
+++ saspy/sasml.py
@@ -18,6 +18,24 @@ from saspy.sasproccommons import SASProcCommons
 
 
 class SASml:
+    """
+    This class is for SAS/STAT procedures to be called as python3 objects and use SAS as the computational engine
+    This class and all the useful work in this package require a licensed version of SAS.
+    To add a new procedure do the following:
+    1. Create a new method for the procedure
+    2. Create the set of required statements. If there are no required statements then create an empty set {}
+    3. Create the legal set of statements. This can often be obtained from the documentation of the procedure.
+        'procopts' should always be included in the legal set to allow flexibility in calling the procedure.
+    4. Create the doc string with the following parts at a minimum:
+        A. Procedure Name
+        B. Required set
+        C. Legal set
+        D. Link to the procedure documentation
+    5. Add the return call for the method using an existing procedure as an example
+    6. Verify that all the statements in the required and legal sets are listed in _makeProcCallMacro method
+        of sasproccommons.py
+    7. Write at least one test to exercise the procedures and include it in the appropriate testing file
+    """
     def __init__(self, session, *args, **kwargs):
         """
         Submit an initial set of macros to prepare the SAS system
