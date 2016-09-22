b3d862649f7333ca804bdfad4a2effe939d4e621
Tom Weber <tom.weber@sas.com>
2016-09-22 09:54:10 -0400

    fix broke install
    Don't understand this yet, but checking for and removing the flag --single-version-externally-managed was keeping the install from working right. So, remove that check. The error was (and there was no saspy directory created in site-packages):
      Running setup.py install for saspy ... done
      Could not find .egg-info directory in install record for saspy from https://pypi.python.org/packages/0a/5a/4d3584c977b9d9f50be39e51c46f5a7905be066932f0198d471c5d8cc8d5/saspy-1.2.1.tar.gz#md5=fb2084944cca3c33015c3cc2efccf7d2

--- setup.py
+++ setup.py
@@ -24,10 +24,6 @@ import sys
 with open('README.rst') as f:
     readme = f.read()
 
-svem_flag = '--single-version-externally-managed'
-if svem_flag in sys.argv:
-    sys.argv.remove(svem_flag)
-
 setup(name='saspy',
       version='1.2.1',
       description='A python interface to SAS',
