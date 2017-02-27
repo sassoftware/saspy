66088bc103fc3b843974501c9cd9113f1ecd3574
Tom Weber <Tom.Weber@sas.com>
2017-02-27 12:54:33 -0500

    keep from eating 100% cpu while waiting on iom

--- saspy/sasioiom.py
+++ saspy/sasioiom.py
@@ -654,6 +654,7 @@ class SASsessionIOM():
                     #print("LIST = \n"+lst)
                     lstf += lst
                  else:
+                    sleep(0.1)
                     try:
                        log = self.stderr[0].recv(4096).decode(self.sascfg.encoding, errors='replace') 
                     except (BlockingIOError):
