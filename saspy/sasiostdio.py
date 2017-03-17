f55d3ada7cbb2a96efd9af4172e0284fd2162c4a
Tom Weber <Tom.Weber@sas.com>
2017-03-17 11:28:50 -0400

    add oserror check in stdio

--- saspy/sasiostdio.py
+++ saspy/sasiostdio.py
@@ -236,6 +236,12 @@ class SASsessionSTDIO():
          try:
             #sleep(5)
             os.execv(pgm, parms)
+         except OSError as e:
+            print("The OS Error was:\n"+e.strerror+'\n')
+            print("SAS Connection failed. No connection established. Double check you settings in sascfg.py file.\n")  
+            print("Attempted to run program "+pgm+" with the following parameters:"+str(parms)+"\n")
+            print("If no OS Error above, try running the following command (where saspy is running) manually to see what is wrong:\n"+s+"\n")
+            os._exit(-6)
          except:
             print("Subprocess failed to start. Double check you settings in sascfg.py file.\n")
             os._exit(-6)
