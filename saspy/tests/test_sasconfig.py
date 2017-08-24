cea34e6e1762c801b96a061ddb6951f640fdee24
Jason <jphillips@ua.edu>
2017-08-24 14:40:13 -0500

    add cfg options for ssh port and tunneling; tests

--- saspy/tests/test_sasconfig.py
+++ saspy/tests/test_sasconfig.py
@@ -1,33 +1,66 @@
 import unittest
+from unittest.mock import patch
 import saspy
-
 
 class TestSASconfigObject(unittest.TestCase):
     @classmethod    
     def setUpClass(cls):
-        cls.sas = saspy.SASsession() #cfgname='default')
-        #cls.assertIsInstance(cls.sas, saspy.SASsession, msg="sas = saspy.SASsession(...) failed")
+        pass
 
     @classmethod
     def tearDownClass(cls):
-        if cls.sas:
-           cls.sas._endsas()
-
-
-    def __del__(self, *args):
-        if self.sas:
-           self.sas._endsas()
+        pass
 
     def setUp(self):
-        #self.sas = saspy.SASsession()
         pass
 
     def tearDown(self):
-        #if self.sas:
-        #   self.sas._endsas()
         pass
 
     def test_SASconfig(self):
-        pass
+        # basic mock configuration
+        bare_config = dict(
+            saspath= '/fake/sas/path'
+        )
+
+        with patch.multiple('saspy.sasbase.SAScfg', default=bare_config):
+            sascfg = saspy.sasiostdio.SASconfigSTDIO(sascfgname='default')
+
+            self.assertEqual(sascfg.saspath, bare_config['saspath'], msg=u'saspath config was not used')
+
+    def test_ssh_config(self):
+        # simple ssh mock configuration
+        simple_ssh =dict(
+            saspath= '/opt/sasinside/SASHome/SASFoundation/9.4/bin/sas_u8',
+            ssh= '/bin/ssh',
+            host= 'hostname',
+        )
+
+        with patch.multiple('saspy.sasbase.SAScfg', ssh=simple_ssh, SAS_config_names=['ssh'], create=True):
+            sascfg = saspy.sasiostdio.SASconfigSTDIO(sascfgname='ssh')
+            pgm, params = saspy.sasiostdio.SASsessionSTDIO._buildcommand({}, sascfg)
+
+            self.assertEqual(pgm, simple_ssh['ssh'], msg=u'ssh config was not used')
+            self.assertNotIn('-R', params, msg=u'ssh tunnel was used though not configured')
+            self.assertNotIn('-p', params, msg=u'ssh alternate port was used though not configured')
+
+        # mock ssh config with additional options
+        ssh_config =dict(
+            saspath= '/opt/sasinside/SASHome/SASFoundation/9.4/bin/sas_u8',
+            ssh= '/bin/ssh-alt',
+            host= 'hostname',
+            port= 9922,
+            tunnel= 9911,
+            encoding= 'latin1'
+        )
+
+        with patch.multiple('saspy.sasbase.SAScfg', ssh=ssh_config, SAS_config_names=['ssh'], create=True):
+            sascfg = saspy.sasiostdio.SASconfigSTDIO(sascfgname='ssh')
+            pgm, params = saspy.sasiostdio.SASsessionSTDIO._buildcommand({}, sascfg)
+            joined = ' '.join(params)
+
+            self.assertEqual(pgm, ssh_config['ssh'], msg=u'ssh config was not used')
+            self.assertIn('-R 9911:localhost:9911', joined, msg=u'ssh tunnel config was not used')
+            self.assertIn('-p 9922', joined, msg=u'ssh port config was not used')
 
 
