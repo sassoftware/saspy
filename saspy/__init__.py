3087f08c666bd15146d7d6a721b23a566bdab9a0
Jared Dean <Jared.Dean@sas.com>
2017-08-25 15:10:58 -0400

    load sasmagic by default in notebook context

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -24,3 +24,19 @@ from saspy.sasproccommons import *
 from saspy.SASLogLexer import *
 
 __version__ = '2.1.1'
+
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+if isnotebook():
+	from .sas_magic import SASMagic
+	get_ipython().register_magics(SASMagic)
