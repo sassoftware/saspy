4be0434e5ccac37e9fe5d9fba6e7f096547fa314
Jared <jared.dean@gmail.com>
2017-08-30 14:01:56 -0400

    Autoloadsas magic (#55)
    * add methods for phreg, ttest, and factor
    
    add the new statements to sasproccommons
    
    * add tests for phreg, ttest, and factor
    
    * Add function to determine if python running in notebook
    
    * load sas_magic in notebook

--- saspy/__init__.py
+++ saspy/__init__.py
@@ -22,4 +22,20 @@ from saspy.sasresults     import *
 from saspy.sasutil        import *
 from saspy.sasproccommons import *
 from saspy.SASLogLexer    import *
-from saspy.version        import __version__
+from saspy.version        import __version__ 
+
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+if isnotebook():
+	from .sas_magic import SASMagic
+	get_ipython().register_magics(SASMagic)
