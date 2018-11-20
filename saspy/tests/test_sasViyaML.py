8deb3dacc0985d06eaf5e3afb895ff083dca036a
Jared <jared.dean@sas.com>
2018-11-20 11:25:46 -0500

    create smoke tests for ViyaML

--- saspy/tests/test_sasViyaML.py
+++ saspy/tests/test_sasViyaML.py
@@ -4,88 +4,60 @@ from saspy.tests.util import Utilities
 
 
 class TestSASViyaML(unittest.TestCase):
-    def setUp(self):
-        # Use the first entry in the configuration list
-        self.sas = saspy.SASsession(cfgname=saspy.SAScfg.SAS_config_names[0])
-        self.assertIsInstance(self.sas, saspy.SASsession, msg="sas = saspy.SASsession(...) failed")
-        self.util = Utilities(self.sas)
+    @classmethod
+    def setUpClass(cls):
+        cls.sas = saspy.SASsession()
+        util = Utilities(cls.sas)
         procNeeded = ['factmac', 'fastknn', 'forest', 'gradboost', 'nnet', 'svdd', 'svmachine']
-        if not self.util.procFound(procNeeded):
-            self.skipTest("Not all of these procedures were found: %s" % str(procNeeded))
+        if not util.procFound(procNeeded):
+            cls.skipTest("Not all of these procedures were found: %s" % str(procNeeded))
 
-    def tearDown(self):
-        if self.sas:
-            self.sas._endsas()
+    @classmethod
+    def tearDownClass(cls):
+        if cls.sas:
+            cls.sas._endsas()
 
     def testFactmacSmoke1(self):
         ml = self.sas.sasviyaml()
         dt = self.sas.sasdata("class", "sashelp")
         out1 = ml.factmac(data=dt, target='height', input={'interval': 'weight', "nominal": 'sex'})
-        a = ['BASELINE', 'DATAACCESSINFO', 'FITSTATISTICS', 'LOG',
-             'MODELINFO', 'NOBS', 'PERFORMANCEINFO', 'VARIABLEIMPORTANCE']
-        self.assertEqual(sorted(a), sorted(out1.__dir__()),
-                         msg=u"Simple FACTMAC  model failed to return correct objects expected:{0:s}  returned:{1:s}".format(
-                             str(a), str(out1)))
+        self.assertFalse('ERROR_LOG' in out1.__dir__(), msg=u"factmac had errors in the log")
 
     def testFastknnSmoke1(self):
         ml = self.sas.sasviyaml()
         dt = self.sas.sasdata("class", "sashelp")
         out1 = ml.fastknn(data=dt, input={'interval': 'weight', "nominal": 'sex'})
-        a = ['BASELINE', 'DATAACCESSINFO', 'FITSTATISTICS', 'LOG',
-             'MODELINFO', 'NOBS', 'PERFORMANCEINFO', 'VARIABLEIMPORTANCE']
-        self.assertEqual(sorted(a), sorted(out1.__dir__()),
-                         msg=u"Simple FASTKNN  model failed to return correct objects expected:{0:s}  returned:{1:s}".format(
-                             str(a), str(out1)))
+        self.assertFalse('ERROR_LOG' in out1.__dir__(), msg=u"fastknn had errors in the log")
 
     def testForestSmoke1(self):
         ml = self.sas.sasviyaml()
         dt = self.sas.sasdata("class", "sashelp")
         out1 = ml.forest(data=dt, target='height', input={'interval': 'weight', "nominal": 'sex'})
-        a = ['BASELINE', 'DATAACCESSINFO', 'FITSTATISTICS', 'LOG',
-             'MODELINFO', 'NOBS', 'PERFORMANCEINFO', 'VARIABLEIMPORTANCE']
-        self.assertEqual(sorted(a), sorted(out1.__dir__()),
-                         msg=u"Simple FOREST  model failed to return correct objects expected:{0:s}  returned:{1:s}".format(
-                             str(a), str(out1)))
+        self.assertFalse('ERROR_LOG' in out1.__dir__(), msg=u"forest had errors in the log")
 
     def testGradboostSmoke1(self):
         ml = self.sas.sasviyaml()
         dt = self.sas.sasdata("class", "sashelp")
         out1 = ml.gradboost(data=dt, target='height', input={'interval': 'weight', "nominal": 'sex'})
-        a = ['BASELINE', 'DATAACCESSINFO', 'FITSTATISTICS', 'LOG',
-             'MODELINFO', 'NOBS', 'PERFORMANCEINFO', 'VARIABLEIMPORTANCE']
-        self.assertEqual(sorted(a), sorted(out1.__dir__()),
-                         msg=u"Simple GRADBOOST  model failed to return correct objects expected:{0:s}  returned:{1:s}".format(
-                             str(a), str(out1)))
+        self.assertFalse('ERROR_LOG' in out1.__dir__(), msg=u"gradboost had errors in the log")
 
     def testNnetSmoke1(self):
         ml = self.sas.sasviyaml()
         dt = self.sas.sasdata("class", "sashelp")
         out1 = ml.nnet(data=dt, target='height', input={'interval': 'weight', "nominal": 'sex'})
-        a = ['BASELINE', 'DATAACCESSINFO', 'FITSTATISTICS', 'LOG',
-             'MODELINFO', 'NOBS', 'PERFORMANCEINFO', 'VARIABLEIMPORTANCE']
-        self.assertEqual(sorted(a), sorted(out1.__dir__()),
-                         msg=u"Simple NNET model failed to return correct objects expected:{0:s}  returned:{1:s}".format(
-                             str(a), str(out1)))
+        self.assertFalse('ERROR_LOG' in out1.__dir__(), msg=u"nnet had errors in the log")
 
     def testSvddSmoke1(self):
         ml = self.sas.sasviyaml()
         dt = self.sas.sasdata("class", "sashelp")
         out1 = ml.svdd(data=dt, input={'interval': 'weight', "nominal": 'sex'})
-        a = ['BASELINE', 'DATAACCESSINFO', 'FITSTATISTICS', 'LOG',
-             'MODELINFO', 'NOBS', 'PERFORMANCEINFO', 'VARIABLEIMPORTANCE']
-        self.assertEqual(sorted(a), sorted(out1.__dir__()),
-                         msg=u"Simple SVDD model failed to return correct objects expected:{0:s}  returned:{1:s}".format(
-                             str(a), str(out1)))
+        self.assertFalse('ERROR_LOG' in out1.__dir__(), msg=u"svdd had errors in the log")
 
     def testSvmachineSmoke1(self):
         ml = self.sas.sasviyaml()
         dt = self.sas.sasdata("class", "sashelp")
         out1 = ml.svmachine(data=dt, target='height', input={'interval': 'weight', "nominal": 'sex'})
-        a = ['BASELINE', 'DATAACCESSINFO', 'FITSTATISTICS', 'LOG',
-             'MODELINFO', 'NOBS', 'PERFORMANCEINFO', 'VARIABLEIMPORTANCE']
-        self.assertEqual(sorted(a), sorted(out1.__dir__()),
-                         msg=u"Simple SVMACHINE model failed to return correct objects expected:{0:s}  returned:{1:s}".format(
-                             str(a), str(out1)))
+        self.assertFalse('ERROR_LOG' in out1.__dir__(), msg=u"svmachine had errors in the log")
 
 if __name__ == '__main__':
     unittest.main()
