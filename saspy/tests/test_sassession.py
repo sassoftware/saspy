f25de29d3f007292ada1763fd328f848ddb052a5
Harrison Hubbell <harry.hubbell@gmail.com>
2019-01-15 09:20:09 -0500

    Test Updates: test_sassession methods should only assert once

--- saspy/tests/test_sassession.py
+++ saspy/tests/test_sassession.py
@@ -7,25 +7,32 @@ class TestSASsessionObject(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.sas = saspy.SASsession()
+        cls.sas.set_batch(True)
 
     @classmethod
     def tearDownClass(cls):
-        if cls.sas:
-           cls.sas._endsas()
+        cls.sas._endsas()
 
+    def test_sassession(self):
+        self.assertIsInstance(self.sas, saspy.SASsession)
 
-    def test_SASsession(self):
-        self.assertIsInstance(self.sas, saspy.SASsession, msg="sas = saspy.SASsession(...) failed")
-
-    def test_SASsession_exist(self):
-        #test exist true
+    def test_sassession_exist_true(self):
+        """
+        Test method exist returns True for a dataset that exists
+        """
         exists = self.sas.exist('cars', libref='sashelp')
-        self.assertTrue(exists, msg="exists = self.sas.exist(...) failed")
+        self.assertTrue(exists)
 
-        #test exist false
+    def test_sassession_exist_false(self):
+        """
+        Test method exist returns False for a dataset that does not exist
+        """
         exists = self.sas.exist('notable', libref='sashelp')
-        self.assertFalse(exists, msg="exists = self.sas.exist(...) failed")
+        self.assertFalse(exists)
 
+    # FIXME
+    # Test should be done in test_sasdata.py
+    '''
     def test_SASsession_sasdata(self):
         #test sasdata existing
         cars = self.sas.sasdata('cars', libref='sashelp', results='text')
@@ -39,63 +46,97 @@ class TestSASsessionObject(unittest.TestCase):
         ll = self.sas.submit("data notable;x=1;run;")
         exists = self.sas.exist('notable')
         self.assertTrue(exists, msg="exists = self.sas.exist(...) failed")
+    '''
 
-    def test_SASsession_csv(self):
-        # test write and read csv
+    def test_SASsession_csv_read(self):
+        """
+        Test method read_csv properly imports a csv file
+        """
+        EXPECTED = ['1', 'Acura', 'MDX', 'SUV', 'Asia', 'All', '$36,945', '$33,337', '3.5']
 
-        self.sas.set_batch(True)
+        with TemporaryDirectory() as temppath:
+            fname = os.path.join(temppath, 'sas_csv_test.csv')
+            self.sas.write_csv(fname, 'cars', libref='sashelp')
+
+            csvdata = self.sas.read_csv(fname, 'csvcars', results='text')
+
+        ll = csvdata.head()
+
+        rows = ll['LST'].splitlines()
+        retrieved = [x.split() for x in rows]
+
+        self.assertIn(EXPECTED, retrieved, msg="csvcars.head() result didn't contain row 1")
+
+    def test_sassession_csv_write(self):
+        """
+        Test method write_csv properly exports a csv file
+        """
         with TemporaryDirectory() as temppath:
             fname = os.path.join(temppath, 'sas_csv_test.csv')
             log = self.sas.write_csv(fname, 'cars', libref='sashelp')
-            self.assertNotIn("ERROR", log, msg="sas.write_csv() failed")
-            csvdata = self.sas.read_csv(fname, 'csvcars', results='text')
-            ll = csvdata.head()
-        expected = ['1', 'Acura', 'MDX', 'SUV', 'Asia', 'All', '$36,945', '$33,337', '3.5']
-        rows = ll['LST'].splitlines()
-        retrieved = []
-        for i in range(len(rows)):
-           retrieved.append(rows[i].split()[:9])
-        self.assertIn(expected, retrieved, msg="csvcars.head() result didn't contain row 1")
-        
-    def test_SASsession_datasets(self):
-        # test datasets()
-        self.sas.set_batch(True)
+
+        self.assertNotIn("ERROR", log, msg="sas.write_csv() failed")
+
+    def test_sassession_datasets_work(self):
+        """
+        Test method datasets can identify that the WORK library exists
+        """
+        EXPECTED = ['Libref', 'WORK']
+
         log = self.sas.datasets()
-        expected = ['Libref', 'WORK']
         rows = log.splitlines()
-        retrieved = []
-        for i in range(len(rows)):
-           retrieved.append(rows[i].split())
-        self.assertIn(expected, retrieved, msg="cars.datasets() result didn't contain expected result")
+        retrieved = [x.split() for x in rows]
+
+        self.assertIn(EXPECTED, retrieved)
+
+    def test_sassession_datasets_sashelp(self):
+        """
+        Test method datasets can identify that the SASHELP library exists
+        """
+        EXPECTED = ['Libref', 'SASHELP']
 
         log = self.sas.datasets('sashelp')
-        expected = ['Libref', 'SASHELP']
         rows = log.splitlines()
-        retrieved = []
-        for i in range(len(rows)):
-           retrieved.append(rows[i].split())
-           if i > 20:
-              break  # it'll be in the first 20 rows for sure. don't need all of it
-        self.assertIn(expected, retrieved, msg="cars.datasets(...) result didn't contain expected result")
-        
-    def test_SASsession_procobjs(self):
-        # test stat
+        retrieved = [x.split() for x in rows]
+
+        self.assertIn(EXPECTED, retrieved)
+
+    def test_sassession_hasstat(self):
+        """
+        Test method sasstat() returns a SASstat object.
+        """
         stat = self.sas.sasstat()
+
         self.assertIsInstance(stat, saspy.sasstat.SASstat, msg="stat = self.sas.sasstat() failed")
 
-        # test ets
+    def test_sassession_hasets(self):
+        """
+        Test method sasets() returns a SASets object.
+        """
         ets = self.sas.sasets()
+
         self.assertIsInstance(ets, saspy.sasets.SASets, msg="ets = self.sas.sasets() failed")
 
-        # test qc
+    def test_sassession_hasqc(self):
+        """
+        Test method sasqc() returns a SASqc object.
+        """
         qc = self.sas.sasqc()
+
         self.assertIsInstance(qc, saspy.sasqc.SASqc, msg="qc = self.sas.sasqc() failed")
 
-        # test ml
+    def test_sassession_hasml(self):
+        """
+        Test method sasml() returns a SASml object.
+        """
         ml = self.sas.sasml()
+
         self.assertIsInstance(ml, saspy.sasml.SASml, msg="ml = self.sas.sasml() failed")
 
-        # test util
+    def test_sassession_hasutil(self):
+        """
+        Test method sasutil() returns a SASutil object.
+        """
         util = self.sas.sasutil()
-        self.assertIsInstance(util, saspy.sasutil.SASutil, msg="util = self.sas.sasutil() failed")
 
+        self.assertIsInstance(util, saspy.sasutil.SASutil, msg="util = self.sas.sasutil() failed")
