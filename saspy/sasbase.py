2092ba60ff9bb4fb8168578ee87a609a185baac6
Harry Hubbell <harry.hubbell@gmail.com>
2019-04-03 10:57:59 -0400

    Add SASConfigNotValidError and SASIONotSupportedError (#219)
    * Add SASIONotSupportedError
    
    * Do not continue if SASsession failed to set _io
    
    * Add SASConfigNotValidError
    
    * Add tests

--- saspy/sasbase.py
+++ saspy/sasbase.py
@@ -41,14 +41,15 @@ import sys
 import getpass
 import tempfile
 
-from saspy.sasioiom  import SASsessionIOM
-from saspy.sasets    import SASets
-from saspy.sasml     import SASml
-from saspy.sasqc     import SASqc
-from saspy.sasstat   import SASstat
-from saspy.sasutil   import SASutil
-from saspy.sasViyaML import SASViyaML
-from saspy.sasdata   import SASdata
+from saspy.sasioiom      import SASsessionIOM
+from saspy.sasets        import SASets
+from saspy.sasexceptions import SASIONotSupportedError, SASConfigNotValidError
+from saspy.sasml         import SASml
+from saspy.sasqc         import SASqc
+from saspy.sasstat       import SASstat
+from saspy.sasutil       import SASutil
+from saspy.sasViyaML     import SASViyaML
+from saspy.sasdata       import SASdata
 
 try:
    import pandas as pd
@@ -121,25 +122,20 @@ class SASconfig:
 
         self.SAScfg = SAScfg
 
-        # GET Config options
-        try:
-            self.cfgopts = getattr(SAScfg, "SAS_config_options")
-        except:
-            self.cfgopts = {}
+        # Get Config options. Fallback to empty dict.
+        self.cfgopts = getattr(SAScfg, "SAS_config_options", {})
 
-        # in lock down mode, don't allow runtime overrides of option values from the config file.
+        # In lock down mode, don't allow runtime overrides of option values from the config file.
         lock = self.cfgopts.get('lock_down', True)
 
-        # GET Config names
-        configs = getattr(SAScfg, "SAS_config_names")
+        # Get Config names. Fallback to empty list.
+        configs = getattr(SAScfg, "SAS_config_names", [])
 
         cfgname = kwargs.get('cfgname', '')
 
         if len(cfgname) == 0:
             if len(configs) == 0:
-                print("No SAS Configuration names found in saspy.sascfg")
-                self.valid = False
-                return
+                raise SASConfigNotValidError(cfgname, msg='No SAS_config_names found in saspy.sascfg')
             else:
                 if len(configs) == 1:
                     cfgname = configs[0]
@@ -160,13 +156,13 @@ class SASconfig:
         self.name = cfgname
         cfg = getattr(SAScfg, cfgname)
 
-        ip            = cfg.get('ip',       '')
-        ssh           = cfg.get('ssh',      '')
-        path          = cfg.get('saspath',  '')
-        java          = cfg.get('java',     '')
+        ip            = cfg.get('ip')
+        ssh           = cfg.get('ssh')
+        path          = cfg.get('saspath')
+        java          = cfg.get('java')
         self.display  = cfg.get('display',  '')
-        self.results  = cfg.get('results',  None)
-        self.autoexec = cfg.get('autoexec', None)
+        self.results  = cfg.get('results')
+        self.autoexec = cfg.get('autoexec')
 
         indisplay = kwargs.get('display', '')
         if len(indisplay) > 0:
@@ -195,17 +191,16 @@ class SASconfig:
             else:
                 self.autoexec = inautoexec
 
-        if len(java)   > 0:
-            self.mode  = 'IOM'
-        elif len(ip)   > 0:
-            self.mode  = 'HTTP'
-        elif len(ssh)  > 0:
-            self.mode  = 'SSH'
-        elif len(path) > 0:
-            self.mode  = 'STDIO'
+        if java is not None:
+            self.mode = 'IOM'
+        elif ip is not None:
+            self.mode = 'HTTP'
+        elif ssh is not None:
+            self.mode = 'SSH'
+        elif path is not None:
+            self.mode = 'STDIO'
         else:
-            print("Configuration Definition " + cfgname + " is not valid. Failed to create a SASsession.")
-            self.valid = False
+            raise SASConfigNotValidError(cfgname)
 
     def _prompt(self, prompt, pw=False):
         if self._kernel is None:
@@ -316,75 +311,70 @@ class SASsession():
             if os.name != 'nt':
                 self._io = SASsessionSTDIO(sascfgname=self.sascfg.name, sb=self, **kwargs)
             else:
-                print("Cannot use STDIO I/O module on Windows. No "
-                    "SASsession established. Choose an IOM SASconfig " 
-                    "definition")
+                raise SASIONotSupportedError(self.sascfg.mode, alts=['IOM'])
         elif self.sascfg.mode == 'IOM':
             self._io = SASsessionIOM(sascfgname=self.sascfg.name, sb=self, **kwargs)
 
-        try:
-            if self._io.pid:
-                sysvars = """options nosource;
-                    %put WORKPATH=%sysfunc(pathname(work));
-                    %put ENDWORKPATH=;
-                    %put ENCODING=&SYSENCODING;
-                    %put SYSVLONG=&SYSVLONG4;
-                    %put SYSJOBID=&SYSJOBID;
-                    %put SYSSCP=&SYSSCP;
-                    options source;
-                """
-                enc = self._io.sascfg.encoding #validating encoding is done next, so handle it not being set for this one call
-                if enc == '':
-                   self._io.sascfg.encoding = 'utf_8'
-                res = self.submit(sysvars, "text")['LOG']
-                self._io.sascfg.encoding = enc
+        sysvars = """options nosource;
+            %put WORKPATH=%sysfunc(pathname(work));
+            %put ENDWORKPATH=;
+            %put ENCODING=&SYSENCODING;
+            %put SYSVLONG=&SYSVLONG4;
+            %put SYSJOBID=&SYSJOBID;
+            %put SYSSCP=&SYSSCP;
+            options source;
+        """
+        # Validating encoding is done next, so handle it not being set for
+        # this one call
+        enc = self._io.sascfg.encoding
+        if enc == '':
+           self._io.sascfg.encoding = 'utf_8'
+        res = self.submit(sysvars, "text")['LOG']
+        self._io.sascfg.encoding = enc
 
-                vlist         = res.rpartition('SYSSCP=')
-                self.hostsep  = vlist[2].partition('\n')[0]
-                vlist         = res.rpartition('SYSJOBID=')
-                self.SASpid   = vlist[2].partition('\n')[0]
-                vlist         = res.rpartition('SYSVLONG=')
-                self.sasver   = vlist[2].partition('\n')[0]
-                vlist         = res.rpartition('ENCODING=')
-                self.sascei   = vlist[2].partition('\n')[0]
-                vlist         = res.rpartition('\nENDWORKPATH=')
-                self.workpath = vlist[0].rpartition('WORKPATH=')[2].strip().replace('\n','') 
+        vlist         = res.rpartition('SYSSCP=')
+        self.hostsep  = vlist[2].partition('\n')[0]
+        vlist         = res.rpartition('SYSJOBID=')
+        self.SASpid   = vlist[2].partition('\n')[0]
+        vlist         = res.rpartition('SYSVLONG=')
+        self.sasver   = vlist[2].partition('\n')[0]
+        vlist         = res.rpartition('ENCODING=')
+        self.sascei   = vlist[2].partition('\n')[0]
+        vlist         = res.rpartition('\nENDWORKPATH=')
+        self.workpath = vlist[0].rpartition('WORKPATH=')[2].strip().replace('\n','') 
 
-                # validate encoding
-                pyenc = sas_encoding_mapping[self.sascei]
-                if pyenc is not None:
-                   if self._io.sascfg.encoding != '':
-                      if self._io.sascfg.encoding.lower() not in pyenc:
-                         print("The encoding value provided doesn't match the SAS session encoding.")
-                         print("SAS encoding is "+self.sascei+". Specified encoding is "+self._io.sascfg.encoding+".")
-                         print("Using encoding "+pyenc[0]+" instead to avoid transcoding problems.")
-                         self._io.sascfg.encoding = pyenc[0]
-                         print("You can override this change, if you think you must, by changing the encoding attribute of the SASsession object, as follows.")
-                         print("""If you had 'sas = saspy.SASsession(), then submit: "sas._io.sascfg.encoding='override_encoding'" to change it""")
-                   else:
-                      self._io.sascfg.encoding = pyenc[0]
-                      if self._io.sascfg.verbose:
-                         print("No encoding value provided. Will try to determine the correct encoding.")
-                         print("Setting encoding to "+pyenc[0]+" based upon the SAS session encoding value of "+self.sascei+".")
-                else:
-                   print("The SAS session encoding for this session ("+self.sasce+") doesn't have a known Python equivalent encoding.")
-                   if self._io.sascfg.encoding == '':
-                      self._io.sascfg.encoding  = 'utf_8'
-                      print("Proceeding using the default encoding of 'utf_8', though you may encounter transcoding problems.")
-                   else:
-                      print("Proceeding using the specified encoding of "+self._io.sascfg.encoding+", though you may encounter transcoding problems.")
+        # validate encoding
+        pyenc = sas_encoding_mapping[self.sascei]
+        if pyenc is not None:
+           if self._io.sascfg.encoding != '':
+              if self._io.sascfg.encoding.lower() not in pyenc:
+                 print("The encoding value provided doesn't match the SAS session encoding.")
+                 print("SAS encoding is "+self.sascei+". Specified encoding is "+self._io.sascfg.encoding+".")
+                 print("Using encoding "+pyenc[0]+" instead to avoid transcoding problems.")
+                 self._io.sascfg.encoding = pyenc[0]
+                 print("You can override this change, if you think you must, by changing the encoding attribute of the SASsession object, as follows.")
+                 print("""If you had 'sas = saspy.SASsession(), then submit: "sas._io.sascfg.encoding='override_encoding'" to change it""")
+           else:
+              self._io.sascfg.encoding = pyenc[0]
+              if self._io.sascfg.verbose:
+                 print("No encoding value provided. Will try to determine the correct encoding.")
+                 print("Setting encoding to "+pyenc[0]+" based upon the SAS session encoding value of "+self.sascei+".")
+        else:
+           print("The SAS session encoding for this session ("+self.sasce+") doesn't have a known Python equivalent encoding.")
+           if self._io.sascfg.encoding == '':
+              self._io.sascfg.encoding  = 'utf_8'
+              print("Proceeding using the default encoding of 'utf_8', though you may encounter transcoding problems.")
+           else:
+              print("Proceeding using the specified encoding of "+self._io.sascfg.encoding+", though you may encounter transcoding problems.")
 
-                if self.hostsep == 'WIN':
-                    self.hostsep = '\\'
-                else:
-                    self.hostsep = '/'
-                self.workpath = self.workpath + self.hostsep
+        if self.hostsep == 'WIN':
+            self.hostsep = '\\'
+        else:
+            self.hostsep = '/'
+        self.workpath = self.workpath + self.hostsep
 
-                if self.sascfg.autoexec:
-                    self.submit(self.sascfg.autoexec)
-
-        except (AttributeError):
-            self._io = None
+        if self.sascfg.autoexec:
+            self.submit(self.sascfg.autoexec)
 
     def __repr__(self):
         """
@@ -414,7 +404,7 @@ class SASsession():
         return x
 
     def __del__(self):
-        if self._io:
+        if getattr(self, '_io', None) is not None:
            return self._io.__del__()
 
     def _objcnt(self):
---
+++ saspy/tests/test_sasexceptions.py
@@ -0,0 +1,117 @@
+from unittest import mock
+import unittest
+import saspy
+import sys
+import tempfile
+import os
+
+
+CONFIG_STDIO = """
+SAS_config_names = ['default']
+default = {'saspath': '/opt/sasinside/SASHome/SASFoundation/9.4/bin/sas_u8'}
+"""
+CONFIG_SSH = """
+SAS_config_names = ['ssh']
+ssh = {'saspath': '/opt/sasinside/SASHome/SASFoundation/9.4/bin/sas_en',
+    'ssh': '/usr/bin/ssh',
+    'host': 'remote.linux.host',
+    'encoding': 'latin1',
+    'options': ["-fullstimer"]}
+"""
+CONFIG_IOMWIN = """
+SAS_config_names = ['iomwin']
+iomwin = {'java': '/usr/bin/java',
+    'iomhost': 'windows.iom.host',
+    'iomport': 8591,
+    'encoding': 'windows-1252',
+    'classpath': '/dummy/path/to/saspyiom.jar'}
+"""
+CONFIG_INVALID = """
+SAS_config_names = ['not_supported']
+not_supported = {'whatever': 'some value'}
+"""
+
+
+class TestSASExceptions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Create dummy config files for test cases. Use `NamedTemporaryFile`
+        instead of in-memory `StringIO` because `SASsession` expects a file
+        path, not a file-like object when passed a `cfgfile`.
+        """
+        # STDIO config file
+        with tempfile.NamedTemporaryFile('w', delete=False) as tf:
+            tf.write(CONFIG_STDIO)
+            cls.config_stdio = tf.name
+
+        # SSH config file
+        with tempfile.NamedTemporaryFile('w', delete=False) as tf:
+            tf.write(CONFIG_SSH)
+            cls.config_ssh = tf.name
+
+        # Windows IOM config file
+        with tempfile.NamedTemporaryFile('w', delete=False) as tf:
+            tf.write(CONFIG_IOMWIN)
+            cls.config_iomwin = tf.name
+
+        # Invalid config file
+        with tempfile.NamedTemporaryFile('w', delete=False) as tf:
+            tf.write(CONFIG_INVALID)
+            cls.config_invalid = tf.name
+
+        # Empty config file
+        with tempfile.NamedTemporaryFile('w', delete=False) as tf:
+            tf.write('')
+            cls.config_empty = tf.name
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Clean up named temp files
+        """
+        os.unlink(cls.config_stdio)
+        os.unlink(cls.config_ssh)
+        os.unlink(cls.config_iomwin)
+        os.unlink(cls.config_invalid)
+        os.unlink(cls.config_empty)
+
+    def tearDown(self):
+        """
+        Remove `sascfgfile` module after test.
+        """
+        del sys.modules['sascfgfile']
+
+    @mock.patch('os.name', 'nt')
+    def test_raises_SASIONotSupportedError_stdio(self):
+        """
+        Test passing STDIO config option on Windows raises
+        SASIONotSupportedError. Patch os.name to always return 'nt'
+        even on non-Windows systems.
+        """
+        with self.assertRaises(saspy.SASIONotSupportedError):
+            sas = saspy.SASsession(cfgfile=self.config_stdio)
+
+    @mock.patch('os.name', 'nt')
+    def test_raises_SASIONotSupportedError_ssh(self):
+        """
+        Test passing STDIO config option on Windows raises
+        SASIONotSupportedError. Patch os.name to always return 'nt'
+        even on non-Windows systems.
+        """
+        with self.assertRaises(saspy.SASIONotSupportedError):
+            sas = saspy.SASsession(cfgfile=self.config_ssh)
+
+    def test_raises_SASConfigNotValidError_invalid(self):
+        """
+        Test that passing an invalid config raises SASConfigNotValidError.
+        """
+        with self.assertRaises(saspy.SASConfigNotValidError):
+            sas = saspy.SASsession(cfgfile=self.config_invalid)
+
+    def test_raises_SASConfigNotValidError_empty(self):
+        """
+        Test that passing an empty config raises SASConfigNotValidError.
+        """
+        with self.assertRaises(saspy.SASConfigNotValidError):
+            sas = saspy.SASsession(cfgfile=self.config_empty)
