724f2f1175ab25fee43594756bcc742090c1cf91
Tom Weber <Tom.Weber@sas.com>
2021-06-18 10:46:25 -0400

    change forkpty to fork to test out

--- saspy/sasioiom.py
+++ saspy/sasioiom.py
@@ -416,12 +416,14 @@ Will use HTML5 for this SASsession.""")
          pout = os.pipe()
          perr = os.pipe()
 
+         pidpty = (os.fork(),0)
+         '''
          try:
             pidpty = os.forkpty()
          except:
             import pty
             pidpty = pty.fork()
-
+         '''
          if pidpty[0]:
             # we are the parent
             self.pid = pidpty[0]
--- saspy/sasiostdio.py
+++ saspy/sasiostdio.py
@@ -325,12 +325,14 @@ Will use HTML5 for this SASsession.""")
          pout = os.pipe()
          perr = os.pipe()
 
+         pidpty = (os.fork(),0)
+         '''
          try:
             pidpty = os.forkpty()
          except:
             import pty
             pidpty = pty.fork()
-
+         '''
          if pidpty[0]:
             # we are the parent
 
