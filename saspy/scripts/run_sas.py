be0d697c33d4e2372364bcd1b0694c5b3dabdd2a
Damjan Krstajic <damjan.krstajic@rcc.org.rs>
2021-07-19 21:32:40 +0200

    run_sas.py: added -r option

--- saspy/scripts/run_sas.py
+++ saspy/scripts/run_sas.py
@@ -9,13 +9,20 @@ import sys
 
 # Usage:
 # ./run_sas.py -s example_1.sas 
-# ./run_sas.py -s example_1.sas -l saspy.log -o saspy.lst
+# ./run_sas.py -s example_1.sas -l out1.log -o out1.lst
+# ./run_sas.py -s example_1.sas -r TEXT
+# ./run_sas.py -s example_1.sas -r HTML 
+# ./run_sas.py -s example_1.sas -r HTML -l out2.log -o out2.html 
+# ./run_sas.py -s example_1.sas -r TEXT -l out3.log -o out3.lst 
+# ./run_sas.py -s /home/a/b/c/example_1.sas 
+ 
 
 def main():
     parser = argparse.ArgumentParser(description="Execute SAS code using saspy.")
     parser.add_argument('-s', '--sas_fname',help='name of the SAS file to be executed')
     parser.add_argument('-l', '--log_fname', help='name of the output LOG file name')
     parser.add_argument('-o', '--lst_fname', help='name of the output LST file')
+    parser.add_argument('-r', '--results_format', help='results format for sas_session.submit(). It may be either TEXT or HTML. If not specified it is TEXT by default')
     options = parser.parse_args()
 
     if options.sas_fname is None:
@@ -34,9 +41,20 @@ def main():
     else:
         log_fname = options.log_fname
 
+    if options.results_format is None:
+        results_format = 'TEXT' 
+    elif options.results_format in ('HTML','TEXT'):
+        results_format = options.results_format
+    else:
+        parser.print_help()
+        sys.exit(0)
+
     if options.lst_fname is None:
-        base      = os.path.basename(sas_fname)
-        lst_fname = os.path.splitext(base)[0] + ".lst"
+        base = os.path.basename(sas_fname)
+        if results_format == 'HTML':
+            lst_fname = os.path.splitext(base)[0] + ".html"
+        else:
+            lst_fname = os.path.splitext(base)[0] + ".lst"
         print("lst_fname is " + lst_fname )
     else:
         lst_fname = options.lst_fname
@@ -47,7 +65,7 @@ def main():
 
     sas_session = saspy.SASsession()
     
-    c = sas_session.submit(sas_code_txt,results="TEXT")
+    c = sas_session.submit(sas_code_txt,results=results_format)
 
     with open(log_fname, 'w') as f1:
         f1.write(c["LOG"])
