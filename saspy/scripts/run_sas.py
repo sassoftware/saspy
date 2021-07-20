de958197b41086c47ddd992de31ee1a9f6605be3
damjankrstajic <80286739+damjankrstajic@users.noreply.github.com>
2021-07-20 15:55:11 +0200

    add run_sas.py script (#392)
    * initial version
    
    * run_sas.py: default the filenames for log and lst to be the name of the input file, with .sas removed and .log/.lst added
    
    * run_sas.py: write() instead of redirecting stdout
    
    * run_sas.py: added -r option
    
    * run_sas.py: now creates output files in the same directory as the .sas file
    
    * run_sas.py: -r argument is now case insensitive
    
    * run_sas.py: added -c option

---
+++ saspy/scripts/run_sas.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+# Author: Damjan Krstajic
+
+import argparse
+import saspy
+import os.path
+import sys
+
+# Usage:
+# ./run_sas.py -s example_1.sas 
+# ./run_sas.py -s example_1.sas -l out1.log -o out1.lst
+# ./run_sas.py -s example_1.sas -r TEXT
+# ./run_sas.py -s example_1.sas -r HTML 
+# ./run_sas.py -s example_1.sas -r htMl -l out2.log -o out2.html 
+# ./run_sas.py -s example_1.sas -r teXt -l out3.log -o out3.lst 
+# ./run_sas.py -s /home/a/b/c/example_1.sas 
+# ./run_sas.py -s example_1.sas -r text -l out4.log -o out4.lst -c ssh
+ 
+
+def main():
+    parser = argparse.ArgumentParser(description="Execute SAS code using saspy.")
+    parser.add_argument('-s', '--sas_fname',help='name of the SAS file to be executed')
+    parser.add_argument('-l', '--log_fname', help='name of the output LOG file name')
+    parser.add_argument('-o', '--lst_fname', help='name of the output LST file')
+    parser.add_argument('-r', '--results_format', help='results format for sas_session.submit(). It may be either TEXT or HTML. If not specified it is TEXT by default')
+    parser.add_argument('-c', '--cfgname', help='name of the Configuration Definition to use for the SASsession')
+    options = parser.parse_args()
+
+    if options.sas_fname is None:
+        parser.print_help()
+        sys.exit(0)
+    elif(not os.path.isfile(options.sas_fname)):
+        print("\nSAS file does not exist\n")
+        sys.exit(0)
+
+    sas_fname = options.sas_fname
+
+    if options.log_fname is None:
+        log_fname = os.path.splitext(sas_fname)[0] + ".log"
+        print("log_fname is " + log_fname )
+    else:
+        log_fname = options.log_fname
+
+    if options.results_format is None:
+        results_format = 'TEXT' 
+    elif options.results_format.upper() in ('HTML','TEXT'):
+        results_format = options.results_format
+    else:
+        parser.print_help()
+        sys.exit(0)
+
+    if options.lst_fname is None:
+        if results_format == 'HTML':
+            lst_fname = os.path.splitext(sas_fname)[0] + ".html"
+        else:
+            lst_fname = os.path.splitext(sas_fname)[0] + ".lst"
+        print("lst_fname is " + lst_fname )
+    else:
+        lst_fname = options.lst_fname
+
+    sas_file     = open(sas_fname,mode='r')
+    sas_code_txt = sas_file.read()
+    sas_file.close()
+
+    if options.cfgname is None:
+        sas_session = saspy.SASsession()   
+    else:
+        sas_session = saspy.SASsession(cfgname=options.cfgname)
+
+    c = sas_session.submit(sas_code_txt,results=results_format)
+
+    with open(log_fname, 'w') as f1:
+        f1.write(c["LOG"])
+
+    with open(lst_fname, 'w') as f2:
+        f2.write(c["LST"])
+
+    sas_session.endsas()
+
+if __name__ == '__main__':
+    main()
+
