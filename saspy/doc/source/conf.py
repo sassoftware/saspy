9499137d50628b81c5a0cee85c4553232c8c9284
Tom Weber <Tom.Weber@sas.com>
2022-10-03 17:20:06 -0400

    still cleaning up sphinx in newer python

--- saspy/doc/source/conf.py
+++ saspy/doc/source/conf.py
@@ -146,7 +146,11 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    # Toc options
+    'collapse_navigation': False,
+    'navigation_depth': 4,
+}
 
 import sphinx_rtd_theme
 
