e72a555da750b8355a76c4da6687b503caa89895
domcge <don.mcgee@sas.com>
2026-09-18 15:22:06 -0400

    Fixes date/datetime handling in sd2pq(use_arrow=False).  Similar change made in PR #687.
    Signed-off-by: domcge <don.mcgee@sas.com>

--- saspy/sasiostdio.py
+++ saspy/sasiostdio.py
@@ -3452,12 +3452,17 @@ Will use HTML5 for this SASsession.""")
             custom_schema = True
         pandas_kwargs["schema"] = parquet_kwargs["schema"]
 
-        # from_pandas has no string->time64 cast kernel, so those columns are streamed as
-        # strings and routed through _parse_sas_ts_string afterwards instead, as sasdata2arrow
-        # does, rather than letting from_pandas attempt (and fail) the cast itself.
+        # from_pandas has no string->timestamp/string->time64/string->date32 cast kernel,
+        # so those columns are streamed as strings and routed through _parse_sas_ts_string
+        # afterwards instead, as sasdata2arrow does, rather than letting from_pandas attempt
+        # (and fail) the cast itself.
+        timestamp_idx = [i for i in range(nvars) if pa.types.is_timestamp(parquet_kwargs["schema"].field(dvarlist[i]).type)]
         time64_idx = [i for i in range(nvars) if pa.types.is_time(parquet_kwargs["schema"].field(dvarlist[i]).type)]
-        if time64_idx:
-            pandas_kwargs["schema"] = pa.schema([pa.field(f.name, pa.string()) if i in time64_idx else f
+        date32_idx = [i for i in range(nvars) if pa.types.is_date32(parquet_kwargs["schema"].field(dvarlist[i]).type)]
+
+        use_str_idx = timestamp_idx + time64_idx + date32_idx
+        if use_str_idx:
+            pandas_kwargs["schema"] = pa.schema([pa.field(f.name, pa.string()) if i in use_str_idx else f
                                                   for i, f in enumerate(parquet_kwargs["schema"])])
 
         ##### START STERAM #####
@@ -3542,10 +3547,19 @@ Will use HTML5 for this SASsession.""")
 
                     pa_table = pa.Table.from_pandas(df,**pandas_kwargs)
 
+                    #manually cast datetime, date, time columns to the correct type, since from_pandas does not support string->timestamp, string->date32, or string->time64 casts
+                    for i in timestamp_idx:
+                        col_name = dvarlist[i]
+                        casted_column = pc.cast(self._sb._parse_sas_ts_string(pa_table.column(col_name), varcat[i], col_name, coerce_timestamp_errors), pa.timestamp('us'))
+                        pa_table = pa_table.set_column(pa_table.column_names.index(col_name), col_name, casted_column)
                     for i in time64_idx:
                         col_name = dvarlist[i]
-                        ts_col = pc.cast(self._sb._parse_sas_ts_string(pa_table.column(col_name), varcat[i], col_name, coerce_timestamp_errors), pa.time64('us'))
-                        pa_table = pa_table.set_column(pa_table.column_names.index(col_name), col_name, ts_col)
+                        casted_column = pc.cast(self._sb._parse_sas_ts_string(pa_table.column(col_name), varcat[i], col_name, coerce_timestamp_errors), pa.time64('us'))
+                        pa_table = pa_table.set_column(pa_table.column_names.index(col_name), col_name, casted_column)
+                    for i in date32_idx:
+                        col_name = dvarlist[i]
+                        casted_column = pc.cast(self._sb._parse_sas_ts_string(pa_table.column(col_name), varcat[i], col_name, coerce_timestamp_errors), pa.date32())
+                        pa_table = pa_table.set_column(pa_table.column_names.index(col_name), col_name, casted_column)
 
                     if not custom_schema:
                         #cast the int64 columns to timestamp
